6228985919758d945acfe85fff898afdd254d50d
Vlad Moroz <hi@vladmoroz.com>
2023-11-03 17:37:03 +0100

    Initial styles

--- src/app/email-password-example/page.tsx
+++ src/app/email-password-example/page.tsx
@@ -4,21 +4,24 @@ import { useFormState } from 'react-dom';
 import { authenticateWithPassword } from './email-password';
 
 export default function EmailPasswordExamplePage() {
-  const [authenticateState, authenticateAction] = useFormState(authenticateWithPassword, {});
+  const [authenticateState, authenticateAction] = useFormState(authenticateWithPassword, {
+    user: null,
+    error: null,
+  });
 
   return (
     <main>
-      <h1>Email + Password example</h1>
+      <h1>Email + Password</h1>
 
       <form action={authenticateAction}>
         <div>
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
+          <input type="email" name="email" id="email" required />
         </div>
 
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" />
+          <input type="password" name="password" id="password" required />
         </div>
 
         <button type="submit">Sign in</button>
